4b72bf78cd73e7b9f3ca6312bfaf897ea63c5987
zhanghongbin <zhanghongbin@lizhi.fm>
2023-02-14 15:31:33 +0800

    测试脚本和短的 prompt

---
+++ egs/aishell3_from_paddle/test.sh
@@ -0,0 +1,13 @@
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+CUDA_VISIBLE_DEVICES=3 python bin/infer_mandarin_phoneme.py \
+  --decoder-dim 256 \
+  --nhead 4 \
+  --num-decoder-layers 6 \
+  --model-name valle \
+  --audio-prompt prompts/SSB00050394.wav \
+  --text-prompt "sil k uang2 c ao3 sp" \
+  --output-dir infer/valle_middle_epoch1 \
+  --checkpoint exp/valle_middle/epoch-1.pt \
+  --text-tokens data/tokenized/unique_text_tokens.k2symbols \
+  --text "k uang2 c ao3 sil|zh u3 iao4 sh iii4 t a1 x ian4 z ai4 sil|uan3 sh ang4 b u4 j ia1 b an1 d e5 h ua4 sil|j iou4 q v4 d iao4 v2 sil"
